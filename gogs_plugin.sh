703d09298b2009e6a3dfc53f1cf7023e02fd87e0
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2024-01-11 08:45:14 +0300

    WIP

--- gogs_plugin.sh
+++ gogs_plugin.sh
@@ -4,37 +4,34 @@
 # Version: 0.1
 # Date: 2024-01-08
 # License: MIT
+# REF: https://github.com/gogs/go-gogs-client
 ################################################
 function gogs_plugin_version(){ echo "vzool_0.1"; }
 function gogs_check_organization(){
   local domain="$1"
   echo $(curl --write-out '%{http_code}' --silent --output /dev/null -X GET \
-    $HTTP_HOST/orgs/$domain?token=$TOKEN)
+    $HTTP_HOST/api/v1/orgs/$domain?token=$TOKEN)
 }
 function gogs_create_organization(){
   local domain="$1"
-  local username="$2"
   echo $(curl --write-out '%{http_code}' --silent --output /dev/null -X POST \
-        -H "Authorization: Bearer $TOKEN" \
         -H "Content-Type: application/json" \
-        -d '{"username": "'$domain'", "visibility":"private", "full_name": "My Repositores Mirror from '$domain'"}' \
-        $HTTP_HOST/org/users/$username/orgs)
+        -d '{"username": "'$domain'", "full_name": "'$domain'", "description": "My Repositores Mirror from '$domain'"}' \
+        $HTTP_HOST/api/v1/user/orgs?token=$TOKEN)
 }
 function gogs_check_repository(){
   local domain="$1"
   local repository="$2"
   echo $(curl --write-out '%{http_code}' --silent --output /dev/null -X GET \
           -H "Content-Type: application/json" \
-          -H "Authorization: token $TOKEN" $HTTP_HOST/repos/$domain/$repository)
+          -H "Authorization: token $TOKEN" $HTTP_HOST/api/v1/repos/$domain/$repository)
 }
 function gogs_organization_create_repository(){
   local domain="$1"
   local repository="$2"
-  echo $(curl   --write-out '%{http_code}' --silent --output /dev/null \
-                -X 'POST' \
-                -H "Authorization: Bearer $TOKEN" \
+  echo $(curl   --write-out '%{http_code}' --silent --output /dev/null -X 'POST' \
                 -H 'accept: application/json' \
                 -H 'Content-Type: application/json' \
                 --data '{"name":"'$repository'", "private":true, "auto_init": false}' \
-                $HTTP_HOST/admin/users/$domain/repos)
+                $HTTP_HOST/api/v1/org/$domain/repos?token=$TOKEN)
 }
